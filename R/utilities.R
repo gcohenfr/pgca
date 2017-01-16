103dd7853341d4fbaa77a7c95bfc5951d8d6569d
davidkep <david@rotinaj.org>
2017-01-16 15:34:53 -0800

    Add feature to `translate`: new data can be read in from any other directory and/or files

--- R/utilities.R
+++ R/utilities.R
@@ -63,19 +63,23 @@ save.dict <- function(dict, file = stop("`file` must be specified"), ...) {
 #'
 #' Apply the dictionary to the data files and write the translated files to disk.
 #'
-#' The dictionary is applied to the files used to create the dictionary. If
-#' the output directory \code{out.dir} is specified, the translated files will be saved
+#' The dictionary is applied to the data specified in the \code{input} argument.
+#' If the \code{input} argument is missing, the dictionary is applied to the files used
+#' to create the dictionary. The \code{input} argument can either be a directory,
+#' a character vector of file names, or a list of \code{data.frames}s.
+#'
+#' If the output directory \code{out.dir} is specified, the translated files will be saved
 #' in this directory. Otherwise the files will be written to the same directory as the
-#' original file. The function will not overwrite existing files and will fail if the
+#' input files. The function will not overwrite existing files and will fail if the
 #' files already exist. Parameters \code{out.suffix} and \code{out.prefix} can be used
 #' to ensure unique new file names.
-#'
-#' In case the dictionary was built from \code{data.frame}s directly and no output
+#' In case the input is a list of \code{data.frame}s and no output
 #' directory is specified, the \code{data.frame}s will be translated and returned as a
 #' list. The function will also return the translated \code{data.frame}s as list if
 #' the \code{out.dir = NULL}.
 #'
 #' @param dict the PGCA dictionary to use.
+#' @param input input directory, files, or data (see details).
 #' @param out.dir the directory to save the translated files in (see details).
 #'      If \code{NULL}, the translated data frames will be returned directly.
 #' @param out.suffix,out.prefix suffix and prefix that will be added to the translated files.
@@ -96,19 +100,28 @@ save.dict <- function(dict, file = stop("`file` must be specified"), ...) {
 #' )
 #'
 #' # Translate all files in the directory and return as a list of data.frames
-#' trans <- translate(dict, out.dir = NULL)
+#' trans <- translate(dict, input = system.file("extdata", package = "pgca"),
+#'                    out.dir = NULL)
+#'
+#' # Translate only some files in the directory and return as a list of data.frames
+#' trans <- translate(dict, input = c(
+#'                        system.file("extdata", "accs_no_1947.txt", package = "pgca"),
+#'                        ssystem.file("extdata", "accs_no_2007.txt", package = "pgca")
+#'                    ), out.dir = NULL)
 #' str(trans)
 #'
 #' \dontrun{
 #' # Translate all files in the directory and save to another directory
-#' translate(dict, out.dir = "translated")
+#' translate(dict, input = system.file("extdata", package = "pgca"),
+#'           out.dir = "translated")
 #'
 #' # Translate all files in the directory, save to the same directory, but
 #' # add a prefix to the files
-#' translate(dict, out.prefix = "translated_")
+#' translate(dict, input = system.file("extdata", package = "pgca"),
+#'           out.prefix = "translated_")
 #' }
 #'
-translate <- function(dict, out.dir, out.suffix = "", out.prefix = "", col.mapping,
+translate <- function(dict, input, out.dir, out.suffix = "", out.prefix = "", col.mapping,
                       out.pg.col = "PGC") {
     # Validate the column mapping
     col.mapping <- if (missing(col.mapping)) {
@@ -120,8 +133,36 @@ translate <- function(dict, out.dir, out.suffix = "", out.prefix = "", col.mappi
     ##
     ## Read in data
     ##
-    if (!is.null(dict$files)) {
-        files <- dict$files
+    if (missing(input)) {
+        if (!is.null(dict$files)) {
+            input <- dict$files
+        } else {
+            lc <- dict$call
+            lc[[1]] <- quote(list)
+            if (!is.null(lc[["col.mapping"]])) {
+                lc[["col.mapping"]] <- NULL
+            }
+            input <- eval(lc, parent.frame())
+        }
+    }
+
+    if (is.character(input)) {
+        input <- normalizePath(input, mustWork = TRUE)
+        input.finfo <- file.info(input, extra_cols = FALSE)
+
+        dirs <- input[input.finfo$isdir]
+        files <- input[!input.finfo$isdir]
+
+        files <- c(files, unlist(lapply(dirs, function(x) {
+            nodes <- dir(x, full.names = TRUE)
+            dirs.in.dir <- list.dirs(x, recursive = FALSE)
+            setdiff(nodes, dirs.in.dir)
+        })))
+
+        if (length(files) == 0L) {
+            stop("`input` directory is empty")
+        }
+
         dfs <- lapply(files, read.delim, header = TRUE, stringsAsFactors = FALSE)
 
         names(dfs) <- if (!is.null(names(files))) {
@@ -129,14 +170,14 @@ translate <- function(dict, out.dir, out.suffix = "", out.prefix = "", col.mappi
         } else {
             files
         }
-    } else {
-        lc <- dict$call
-        lc[[1]] <- quote(list)
-        if (!is.null(lc[["col.mapping"]])) {
-            lc[["col.mapping"]] <- NULL
+    } else if (is.list(input)) {
+        if (!all(sapply(input, class) == "data.frame")) {
+            stop('one or more list items of `input` are not data.frames.')
         }
-        dfs <- eval(lc, parent.frame())
+        dfs <- input
         files <- NULL
+    } else {
+        stop('`input` not recognized.')
     }
 
     ##
